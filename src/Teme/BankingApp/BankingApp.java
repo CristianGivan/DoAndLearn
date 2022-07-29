d64a20fe0a8a6a418babb76e433c41806de43ba2
Cristi_ROG <cristian.givan.it@gmail.com>
2022-07-29 19:38:58 +0300

    Banking App

--- src/Teme/BankingApp/BankingApp.java
+++ src/Teme/BankingApp/BankingApp.java
@@ -1,9 +1,13 @@
 package Teme.BankingApp;
 
-public class Main {
+public class BankingApp {
     public static void main(String[] args) {
-        BankAccount studentAccount = new StudentAccount(10000, "student1", 50000);
-        BankAccount spendingAccount = new SpendingAccount(10000, "student1", 50000);
+        double amount = 1000;
+        BankAccount studentAccount1 = new StudentAccount(10000, "student1", 50000);
+        BankAccount spendingAccount1 = new SpendingAccount(10000, "spanding1", 50000);
+        BankAccount studentAccount2 = new StudentAccount(10000, "student2", 50000);
+        BankAccount spendingAccount2 = new SpendingAccount(10000, "spanding2", 50000);
+
         //debug student acount
         /*
         System.out.println(studentAccount.getBalance());
@@ -27,6 +31,16 @@ public class Main {
         spendingAccount.withdraw(200000);
         System.out.println(spendingAccount.getBalance());
         */
+
+        Person person = new Person("Ioan", "Popescu", 5);
+        person.addAccount(spendingAccount1);
+        person.addAccount(spendingAccount2);
+        person.addAccount(studentAccount1);
+        person.addAccount(studentAccount2);
+        person.listAccount();
+        person.deposit("student1", amount);
+        person.withdraw("student1", amount);
+
 
     }
 }
--- src/Teme/BankingApp/Person.java
+++ src/Teme/BankingApp/Person.java
@@ -5,7 +5,7 @@ public class Person {
     private String lastName;
 
     private int maximNumberOfAcounts;
-    private int numberOfAcounts;
+    private int numberOfAccounts;
     private BankAccount[] accountsList;
 
     //? cum am putea sa facem o alocare dinamica a numerului de arrayuri
@@ -13,24 +13,56 @@ public class Person {
     public Person(String firstName, String lastName, int maximNumberOfAcounts) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.numberOfAcounts = 0;
+        this.numberOfAccounts = 0;
         this.maximNumberOfAcounts = maximNumberOfAcounts;
-        accountsList = new BankAccount[numberOfAcounts];
+        accountsList = new BankAccount[maximNumberOfAcounts];
+
     }
 
     public boolean addAccount(BankAccount bankAccount) {
-        if (numberOfAcounts < maximNumberOfAcounts) {
-            accountsList[numberOfAcounts] = bankAccount;
-            numberOfAcounts++;
+        if (numberOfAccounts < maximNumberOfAcounts) {
+            accountsList[numberOfAccounts] = bankAccount;
+            numberOfAccounts++;
+//            System.out.println(bankAccount.getAccountNumber());
+//            System.out.println(accountsList[numberOfAccounts].getAccountNumber());
             return true;
         }
         return false;
     }
 
     public void listAccount() {
-        for (int i = 0; i < numberOfAcounts; i++) {
-            System.out.println("Account number " + accountsList[i].getAccountNumber() + " has the balance: " +
-                    accountsList[i].getBalance());
+        for (int i = 0; i < numberOfAccounts; i++) {
+            System.out.println("Account number " + accountsList[i].getAccountNumber() + " has the balance: " + accountsList[i].getBalance());
         }
+    }
+
+    public void deposit(String accountNumber, double amount) {
+        int index = findAccountIndex(accountNumber);
+        double oldBalance = accountsList[index].getBalance();
+        if (accountsList[index].deposit(amount)) {
+            System.out.println("The amount " + amount + " was added to the balance " +
+                    oldBalance + "\n The new balance is " + accountsList[index].getBalance());
+        } else {
+            System.out.println("The amount was not deposited");
+        }
+    }
+    public void withdraw(String accountNumber, double amount) {
+        int index = findAccountIndex(accountNumber);
+        double oldBalance = accountsList[index].getBalance();
+        if (accountsList[index].withdraw(amount)) {
+            System.out.println("The amount " + amount + " was withdraw to the balance " +
+                    oldBalance + "\n The new balance is " + accountsList[index].getBalance());
+        } else {
+            System.out.println("The amount was not deposited");
+        }
+    }
+
+    public int findAccountIndex(String accountNumber) {
+        for (int i = 0; i < numberOfAccounts; i++) {
+            if (accountsList[i].getAccountNumber().equals(accountNumber)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
